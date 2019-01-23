2c0eeb2ee11768d983ad532429e2506b21b3dfb7
Sara Dornblaser <saradornblaser@gmail.com>
2019-01-23 17:58:06 -0500

    firebase connected(?)

---
+++ client/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyC06e0qnZ45BnyVL8mvGC1BC24aoedgRBk',
+  authDomain: 'guava-stackbox.firebaseapp.com',
+  databaseURL: 'https://guava-stackbox.firebaseio.com',
+  projectId: 'guava-stackbox',
+  storageBucket: 'guava-stackbox.appspot.com',
+  messagingSenderId: '173449434888'
+}
+
+firebase.initializeApp(config)
+export default firebase
