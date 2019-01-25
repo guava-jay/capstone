102d532f0320de5c0e3a05e619bb31022b94074a
Sara Dornblaser <saradornblaser@gmail.com>
2019-01-25 17:36:11 -0500

    checks in place to ensure generated slugs are unique

--- server/api/game.js
+++ server/api/game.js
@@ -3,15 +3,34 @@ const database = require('../db/index')
 module.exports = router
 
 router.post('/', (req, res, next) => {
-  const slug = req.body.slug
   try {
-    database
-      .ref(`rooms/`)
-      .child(slug)
-      .set({
-        status: 'waiting'
-      })
-    res.status(201).send(slug)
+    let generateSlug = () => {
+      let arr = []
+      let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+      for (let i = 0; i < 4; i++) {
+        const index = Math.floor(36 * Math.random())
+        arr.push(chars[index])
+      }
+      return arr.join('')
+    }
+
+    let slug = generateSlug()
+    const ref = database.ref(`rooms/`)
+    ref.once('value').then(function(snapshot) {
+      let alreadyThere = snapshot.hasChild(slug)
+      while (alreadyThere) {
+        slug = generateSlug()
+        alreadyThere = snapshot.hasChild(slug)
+      }
+
+      database
+        .ref(`rooms/`)
+        .child(slug)
+        .set({
+          status: 'waiting'
+        })
+      res.status(201).send(slug)
+    })
   } catch (err) {
     console.error(err)
     res.status(500).send('Error creating new game')
