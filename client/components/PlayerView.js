b176b0e3b5168f597731b2e27211f9e2ae113cce
Eve Mendelevich <ym9382@gmail.com>
2019-01-29 12:39:54 -0500

    wip: connect playerView comp with firebase to show current choices

--- client/components/PlayerView.js
+++ client/components/PlayerView.js
@@ -4,7 +4,50 @@ import firebase from '../firebase'
 const database = firebase.database()
 
 export default class PlayerView extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      gameStatus: 'waiting',
+      currentQuestion: null,
+      gameName: null,
+      answerChoices: []
+    }
+    this.setState = this.setState.bind(this)
+  }
+  async componentDidMount() {
+    const currentQuestionRef = database.ref(
+      `/rooms/${this.props.slug}/active_game/current_question`
+    )
+
+    const currentQuestion = await currentQuestionRef
+      .once('value')
+      .then(snapshot => this.setState({currentQuestion: snapshot.val()}))
+
+    // const gameName = await database
+    //   .ref(`/rooms/${this.props.slug}/active_game/game_name`)
+    //   .once('value')
+    //   .then(snapshot => snapshot.val())
+
+    currentQuestionRef.on('value', snapshot => {
+      // console.log(snapshot.val())
+      // const currentChoices = database.ref(`game_list/${gameName}`)
+      if (snapshot.val()) {
+        // this.setState({currentQuestion: snapshot.val()})
+        // console.log(this.state)
+        console.log(snapshot.val())
+      }
+    })
+  }
+
   render() {
-    return <h1> player device waiting for game to start</h1>
+    return (
+      <div>
+        {this.state.currentQuestion ? (
+          <h1>currentQuestion: {this.state.currentQuestion}</h1>
+        ) : (
+          <h1> player device waiting for game to start</h1>
+        )}
+      </div>
+    )
   }
 }
