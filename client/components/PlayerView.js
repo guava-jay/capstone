10583c3c77fa0d669225e54ebdef13d8a4391abb
Eve Mendelevich <ym9382@gmail.com>
2019-01-29 15:31:56 -0500

    feat: playerView tracks changes in current question and updates possible answer choices on state

--- client/components/PlayerView.js
+++ client/components/PlayerView.js
@@ -7,40 +7,50 @@ export default class PlayerView extends React.Component {
     super()
     this.state = {
       gameStatus: 'waiting',
-      currentQuestion: null,
       gameName: null,
+      currentQuestion: null,
       answerChoices: []
     }
     this.setState = this.setState.bind(this)
   }
   async componentDidMount() {
+    // Get reference to current question: used to watch for changes
     const currentQuestionRef = database.ref(
       `/rooms/${this.props.slug}/active_game/current_question`
     )
 
-    const currentQuestion = await currentQuestionRef
-      .once('value')
-      .then(snapshot => {
-        this.setState({currentQuestion: snapshot.val()})
-      })
-
+    // Set inital game name
     const gameName = await database
       .ref(`/rooms/${this.props.slug}/active_game/game_name`)
       .once('value')
-      .then(snapshot => this.setState({gameName: snapshot.val()}))
+      .then(snapshot => snapshot.val())
 
-    const gameStatusRef = await database
+    // Set initial game status
+    const gameStatus = await database
       .ref(`/rooms/${this.props.slug}/status`)
       .once('value')
-      .then(snapshot => this.setState({gameStatus: snapshot.val()}))
+      .then(snapshot => snapshot.val())
 
-    currentQuestionRef.on('value', snapshot => {
+    // Set inital current question
+    const currentQuestion = await currentQuestionRef
+      .once('value')
+      .then(snapshot => snapshot.val())
+
+    this.setState({
+      gameName,
+      gameStatus,
+      currentQuestion
+    })
+
+    // Listens to changes of the currentQuestion
+    currentQuestionRef.on('value', async snapshot => {
       if (snapshot.val()) {
-        const questions = database
-          .ref(`game_list/${this.state.gameName}/${this.state.currentQuestion}`)
+        const answerChoices = await database
+          .ref(`game_list/${this.state.gameName}/${snapshot.val()}/choices`)
           .once('value')
-          .then(snapshot => console.log(snapshot.val()))
-        this.setState({currentQuestion: snapshot.val()})
+          .then(snapshot => snapshot.val())
+
+        this.setState({currentQuestion: snapshot.val(), answerChoices})
       }
     })
   }
@@ -50,7 +60,7 @@ export default class PlayerView extends React.Component {
     return (
       <div>
         {// Change this to this.state.gameStatus
-        this.state.currentQuestion >= 0 ? (
+        this.state.answerChoices ? (
           <h1>currentQuestion: {this.state.currentQuestion}</h1>
         ) : (
           <h1> player device waiting for game to start</h1>
