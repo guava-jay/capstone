3add3d867951c879f4a1f026683758948d76d96d
Eve Mendelevich <ym9382@gmail.com>
2019-01-29 11:40:10 -0500

    refactor: moved players array state to HostView.js from Room.js

--- client/components/HostView.js
+++ client/components/HostView.js
@@ -4,16 +4,43 @@ import firebase from '../firebase'
 const database = firebase.database()
 
 export default class HostView extends React.Component {
-  // constructor(props) {
-  //   super(props)
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+      players: []
+    }
+    this.setState = this.setState.bind(this)
+  }
+
+  componentDidMount() {
+    //players
+    let playersRef = database.ref(`/rooms/${this.props.slug}/players`)
+    playersRef.on(
+      'value',
+      snapshot => {
+        let playerArr = []
+        if (snapshot.val()) {
+          let playerKeys = Object.keys(snapshot.val())
+          playerKeys.forEach(key => {
+            playerArr.push({[key]: snapshot.val()[key]})
+          })
+          this.setState({players: playerArr})
+        } else {
+          this.setState({players: []})
+        }
+      },
+      errorObject => {
+        console.log('The read failed:', errorObject.code)
+      }
+    )
+  }
 
   render() {
     return (
       <div>
         <h1>code: {this.props.slug}</h1>
         <ul>
-          {this.props.players.map((player, i) => {
+          {this.state.players.map((player, i) => {
             let pid = Object.keys(player)[0]
             return <li key={i + ''}> {player[pid].displayName}</li>
           })}
