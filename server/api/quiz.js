3b04a1f812057f1ae5fba8ef4898c3393b63062f
Emily McAllister <esm328@nyu.edu>
2019-01-29 16:01:41 -0500

    set up API route to update the current question

---
+++ server/api/quiz.js
@@ -0,0 +1,53 @@
+const router = require('express').Router()
+const database = require('../db/index')
+const firebase = require('firebase-admin')
+module.exports = router
+
+//slug generator
+let generateSlug = () => {
+  let arr = []
+  let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  for (let i = 0; i < 4; i++) {
+    const index = Math.floor(36 * Math.random())
+    arr.push(chars[index])
+  }
+  return arr.join('')
+}
+
+//fetch a question based on its ID
+router.get(`/:qID`, async (req, res, next) => {})
+
+//change the current question
+//this one will set it randomly, and return the question ID
+//or should it return the question itself?
+//maybe both
+router.put(`/`, async (req, res, next) => {
+  //the touppercase isn't strictly needed but it can't hurt
+  const slug = req.body.slug.toUpperCase()
+  try {
+    //get the number of questions
+    let totalNumberOfQuestions = 0
+    let newQuestionId = 0
+    let newQuestion = {}
+    await database
+      .ref(`game_list/quiz/`)
+      .once('value')
+      .then(snapshot => {
+        totalNumberOfQuestions = snapshot.numChildren()
+        newQuestionId = Math.floor(Math.random() * totalNumberOfQuestions)
+        newQuestion = snapshot.child(newQuestionId).val()
+      })
+    console.log(newQuestion)
+
+    // ref = database.ref(`rooms/${slug}`);
+    await database
+      .ref(`rooms/${slug}/`)
+      .child('current_question')
+      .set(newQuestionId)
+
+    res.status(201).send(newQuestion)
+  } catch (err) {
+    console.error(err)
+    next()
+  }
+})
