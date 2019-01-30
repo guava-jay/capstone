e97abde732ba04358751b2c2e71477f209fb792a
Sara Dornblaser <saradornblaser@gmail.com>
2019-01-30 17:42:21 -0500

    removed test

--- server/api/game.spec.js
+++ server/api/game.spec.js
@@ -2,20 +2,6 @@ const {expect} = require('chai')
 const request = require('supertest')
 const app = require('../index')
 
-const admin = require('firebase-admin')
-if (process.env.NODE_ENV !== 'production') require('../../secrets')
-// Initialize database with global variables
-admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.project_id,
-    clientEmail: process.env.client_email,
-    privateKey: process.env.private_key.replace(/\\n/g, '\n')
-  }),
-  databaseURL: 'https://guava-stackbox.firebaseio.com'
-})
-// Back-end firebase is admin.database()
-const database = admin.database()
-
 describe('game api routes', () => {
   describe('GET api/game', () => {
     it('returns helloworld', async () => {
@@ -34,13 +20,6 @@ describe('game api routes', () => {
       let slug = res.body
       console.log(slug)
       describe('/api/game/join', () => {
-        it('can add users with a valid code', async () => {
-          await request(app)
-            .post('/api/game/join')
-            .send({slug: slug, uid: 'uid', displayName: 'displayName'})
-            .expect(201)
-          await database.ref(`/rooms/${slug}`).remove()
-        })
         it('errors on users with an invalid code', async () => {
           await request(app)
             .post('/api/game/join')
