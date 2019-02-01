32eeee544c9cb91e6e7c1c9acaeab37ddb954ef5
ryuash <ryuash123@gmail.com>
2019-02-01 13:14:43 -0500

    working on welome page css

--- client/components/Welcome.js
+++ client/components/Welcome.js
@@ -5,15 +5,13 @@ import {Navbar} from './'
 const Welcome = () => {
   return (
     <div className="welcome" align="center">
-      <div>
-        <h1>Welcome to Stackbox!</h1>
-        <h3>Where people come to play!</h3>
+      <div id="welcome-title-container">
+        <h1 className="welcome-title">Welcome to Stackbox Games!</h1>
+        <p>Where people come to play!</p>
       </div>
       <div className="welcome-container">
         <Navbar />
-        <div className="box">
-          <img src="/img/cartoon-box.png" />
-        </div>
+        <div className="box">{/* <img src="/img/cartoon-box.png" /> */}</div>
       </div>
     </div>
   )
