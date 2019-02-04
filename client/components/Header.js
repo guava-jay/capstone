01855e23d775c2555fc6529ab3cdcd0f73a3bb7f
Sara Dornblaser <saradornblaser@gmail.com>
2019-02-03 22:23:12 -0500

    playerView reflects when they are deleted and when hostView ends game by navigating to home page

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import {endGameThunk} from '../store/game'
 
 const Header = props => {
+  const deleteCurrentGame = slug => {
+    props.endGameThunk(props.slug)
+  }
   return (
     <h1 id="header">
-      <Link to="/" title="home">
+      <Link to="/" title="home" onClick={deleteCurrentGame}>
         <span className="yellowFont">Stackbox {'\u00A0'}</span>
         <span className="pinkFont">Games</span>
       </Link>
@@ -12,4 +17,15 @@ const Header = props => {
   )
 }
 
-export default Header
+const mapState = state => {
+  console.log('slug', state.game.slug)
+  return {
+    slug: state.game.slug
+  }
+}
+const mapDispatch = dispatch => {
+  return {
+    endGameThunk: slug => dispatch(endGameThunk(slug))
+  }
+}
+export default connect(mapState, mapDispatch)(Header)
