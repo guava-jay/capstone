18e48f0192c58874a1e12040c0bf34d489c825aa
Eve Mendelevich <ym9382@gmail.com>
2019-02-04 15:23:57 -0500

    style: added styling to finished player view mobile

--- client/components/quiz/HostFinished.js
+++ client/components/quiz/HostFinished.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import database from '../../firebase'
-import JoinCreateButtons from '../JoinCreateButtons'
+import {Link} from 'react-router-dom'
 
 export default class HostFinished extends React.Component {
   constructor(props) {
@@ -123,7 +123,16 @@ export default class HostFinished extends React.Component {
       <div>
         <h1>Finished!</h1>
         {endView}
-        <JoinCreateButtons />
+        <div className="buttonContainer" align="center">
+          <nav className="links">
+            <Link to="/">
+              <h4>Back to home</h4>
+            </Link>
+            <Link to="/newGame">
+              <h4>Replay this game</h4>
+            </Link>
+          </nav>
+        </div>
       </div>
     )
   }
--- client/components/quiz/PlayerView.js
+++ client/components/quiz/PlayerView.js
@@ -3,10 +3,9 @@ import React from 'react'
 import database from '../../firebase'
 import {setResponseThunk} from '../../store/user'
 import {connect} from 'react-redux'
-import {NavLink, Redirect} from 'react-router-dom'
+import {Link, NavLink, Redirect} from 'react-router-dom'
 
 import {PieChart, Pie, Cell, Label} from 'recharts'
-import JoinCreateButtons from '../JoinCreateButtons'
 
 class PlayerView extends React.Component {
   constructor() {
@@ -182,27 +181,40 @@ class PlayerView extends React.Component {
       )
     } else if (this.state.gameStatus === 'finished') {
       return (
-        <div>
+        <div id="player-finished">
           <h1 className="center">Results</h1>
-          <PieChart width={400} height={300}>
-            <Pie
-              data={answerData}
-              dataKey="value"
-              nameKey="name"
-              cx="50%"
-              cy="50%"
-              innerRadius={80}
-              outerRadius={100}
-              paddingAngle={0}
-              fill="#8884d8"
-            >
-              <Cell fill="#00C49F" />
-              <Cell fill="#ff3860" />
-              <Label position="center">{`${numCorrect} Correct`}</Label>
-            </Pie>
-          </PieChart>
+          <div id="recharts-container">
+            <PieChart width={200} height={200}>
+              <Pie
+                data={answerData}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                innerRadius={80}
+                outerRadius={100}
+                paddingAngle={0}
+                fill="#8884d8"
+              >
+                <Cell fill="#00C49F" />
+                <Cell fill="#ff3860" />
+                <Label position="center">{`${numCorrect} Correct`}</Label>
+              </Pie>
+            </PieChart>
+          </div>
+          <div className="finished-button-container">
+            <Link to="/">
+              <button className="button6 buttonHome" type="button">
+                <h4>Back to home</h4>
+              </button>
+            </Link>
 
-          <JoinCreateButtons />
+            <Link to="/join">
+              <button className="button6 buttonJoin" type="button">
+                <h4>Join New Game</h4>
+              </button>
+            </Link>
+          </div>
         </div>
       )
     } else if (this.state.gameStatus === 'non-participant') {
