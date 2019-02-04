8b3259e5b345805c3ca4169aa9af31a8be726440
Eve Mendelevich <ym9382@gmail.com>
2019-02-04 16:07:44 -0500

    style: added PlayerView styling for view when room is lost upon host disconnection

--- client/components/quiz/PlayerView.js
+++ client/components/quiz/PlayerView.js
@@ -237,9 +237,23 @@ class PlayerView extends React.Component {
       // Change this to "is_active_game"
     } else if (!this.state.gameStatus) {
       return (
-        <div>
-          <h1>Game has been ended</h1>
-          <NavLink to="/">Play again</NavLink>
+        <div id="player-disconnected">
+          <div>
+            <h1>The host has disconnected.</h1>
+          </div>
+          <div className="finished-button-container">
+            <Link to="/">
+              <button className="button6 buttonHome" type="button">
+                <h4>Back to home</h4>
+              </button>
+            </Link>
+
+            <Link to="/join">
+              <button className="button6 buttonJoin" type="button">
+                <h4>Join New Game</h4>
+              </button>
+            </Link>
+          </div>
         </div>
       )
     } else {
