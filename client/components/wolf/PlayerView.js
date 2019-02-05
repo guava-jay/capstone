277e1c650a42b8aea3157e21b34254456662920e
Emily McAllister <esm328@nyu.edu>
2019-02-05 12:39:05 -0500

    voting now correctly displays options and allows for selection

--- client/components/wolf/PlayerView.js
+++ client/components/wolf/PlayerView.js
@@ -4,45 +4,45 @@ import {setResponseThunk} from '../../store/user'
 import {connect} from 'react-redux'
 import {NavLink, Redirect} from 'react-router-dom'
 import Navbar from '../navbar'
+import {Voting} from '../../components'
 
 class PlayerView extends React.Component {
   constructor() {
     super()
     this.state = {
       gameStatus: 'waiting',
-      gameName: null,
-      currentQuestion: null,
-      otherPlayers: [],
-      responses: {}
+      voting: false,
+      role: '',
+      otherPlayers: []
     }
     this.setState = this.setState.bind(this)
-    this.setChoice = this.setChoice.bind(this)
-    this.submitChoice = this.submitChoice.bind(this)
+    // this.setChoice = this.setChoice.bind(this)
+    // this.submitChoice = this.submitChoice.bind(this)
   }
 
   // Can select between an answer multiple times...
-  setChoice(event) {
-    const currentQuestion = this.state.currentQuestion
-    const currentResponse = event.target.value
-    const responses = {
-      ...this.state.responses,
-      [currentQuestion]: currentResponse
-    }
-    this.setState({responses})
-  }
+  // setChoice(event) {
+  //   const currentQuestion = this.state.currentQuestion
+  //   const currentResponse = event.target.value
+  //   const responses = {
+  //     ...this.state.responses,
+  //     [currentQuestion]: currentResponse
+  //   }
+  //   this.setState({responses})
+  // }
 
   // ...But can only submit once per question
-  submitChoice(event) {
-    event.preventDefault()
+  // submitChoice(event) {
+  //   event.preventDefault()
 
-    const slug = this.props.slug
-    const uid = this.props.user.uid
+  //   const slug = this.props.slug
+  //   const uid = this.props.user.uid
 
-    const currentQuestion = this.state.currentQuestion
-    const answer = this.state.responses[currentQuestion]
-    this.props.setResponseThunk(slug, uid, answer)
-    this.setState({answeredCurrent: true})
-  }
+  //   const currentQuestion = this.state.currentQuestion
+  //   const answer = this.state.responses[currentQuestion]
+  //   this.props.setResponseThunk(slug, uid, answer)
+  //   this.setState({answeredCurrent: true})
+  // }
 
   async initializeState() {
     const ROOM = `/rooms/${this.props.slug}`
@@ -53,33 +53,23 @@ class PlayerView extends React.Component {
       `${ROOM}/players/${this.props.user.uid}`
     )
 
-    // Get reference to current question: used to watch for changes
-    const currentQuestionRef = database.ref(`${ACTIVE_GAME}/current_question`)
-
+    const allPlayers = await database.ref(`${ROOM}/players/`)
     // Get reference to game status
     const gameStatusRef = await database.ref(`${ROOM}/status`)
-
-    // Get inital game name
-    const gameName = await database
-      .ref(`${ACTIVE_GAME}/game_name`)
-      .once('value')
-      .then(snapshot => snapshot.val())
 
     // Get initial game status
     const gameStatus = await gameStatusRef
       .once('value')
       .then(snapshot => snapshot.val())
 
-    // Get inital current question
-    const currentQuestion = await currentQuestionRef
+    const otherPlayers = await allPlayers
       .once('value')
       .then(snapshot => snapshot.val())
 
     // Set state based on db
     this.setState({
-      gameName,
       gameStatus,
-      currentQuestion
+      otherPlayers
     })
 
     // Listens for changes in players; if this particular player is missing, render no-longer-playing screen
@@ -88,32 +78,35 @@ class PlayerView extends React.Component {
         this.setState({gameStatus: 'non-participant'})
       }
     })
-
-    // Listens to changes of the currentQuestion
-    await currentQuestionRef.on('value', async snapshot => {
-      if (snapshot.val() >= 0) {
-        const answerChoices = await database
-          .ref(`game_list/${this.state.gameName}/${snapshot.val()}/choices`)
-          .once('value')
-          .then(snapshot => snapshot.val())
-        this.setState({
-          currentQuestion: snapshot.val(),
-          answerChoices,
-          answeredCurrent: false
-        })
-      }
+    await allPlayers.on('value', snapshot => {
+      this.setState({otherPlayers: snapshot.val()})
     })
+
+    // // Listens to changes of the currentQuestion
+    // await currentQuestionRef.on('value', async snapshot => {
+    //   if (snapshot.val() >= 0) {
+    //     const answerChoices = await database
+    //       .ref(`game_list/${this.state.gameName}/${snapshot.val()}/choices`)
+    //       .once('value')
+    //       .then(snapshot => snapshot.val())
+    //     this.setState({
+    //       currentQuestion: snapshot.val(),
+    //       answerChoices,
+    //       answeredCurrent: false
+    //     })
+    //   }
+    // })
 
     // Listens to changes of the gameStatus
     gameStatusRef.on('value', snapshot => {
       this.setState({gameStatus: snapshot.val()})
     })
 
-    database
-      .ref(`${ROOM}/players/${this.props.user.uid}/currentScore`)
-      .on('value', snapshot => {
-        this.setState({currentScore: snapshot.val()})
-      })
+    // database
+    //   .ref(`${ROOM}/players/${this.props.user.uid}/currentScore`)
+    //   .on('value', snapshot => {
+    //     this.setState({currentScore: snapshot.val()})
+    //   })
   }
 
   componentDidMount() {
@@ -127,76 +120,14 @@ class PlayerView extends React.Component {
   }
 
   render() {
-    const numCorrect = this.state.currentScore
-    const numIncorrect = Object.keys(this.state.responses).length - numCorrect
-
-    const answerData = [
-      {name: 'Correct', value: numCorrect},
-      {name: 'Incorrect', value: numIncorrect}
-    ]
-
     // We want to disable the submit button if there has been no selected response OR if the player has already selected a response
-    const NoSelectedCurrent = !this.state.responses[this.state.currentQuestion]
+    // const NoSelectedCurrent = !this.state.responses[this.state.currentQuestion]
     if (this.state.gameStatus === 'waiting') {
       return <h1 id="player-waiting">Waiting to start...</h1>
-    } else if (
-      this.state.gameStatus === 'playing' &&
-      this.state.answerChoices
-    ) {
+    } else if (this.state.gameStatus === 'playing') {
       //show answer choices
       return (
-        <React.Fragment>
-          {this.state.answeredCurrent ? (
-            <h1 id="player-submit">submitted!</h1>
-          ) : (
-            <div id="player-choice-container">
-              <h2>Choose carefully...</h2>
-              <form onSubmit={this.submitChoice} onChange={this.setChoice}>
-                {this.state.answerChoices.map(choice => (
-                  <label key={choice}>
-                    <input type="radio" name="choices" value={choice} />
-                    <p>{choice}</p>
-                  </label>
-                ))}
-                <br />
-                <button
-                  title="submit answer"
-                  className="button6"
-                  type="submit"
-                  disabled={NoSelectedCurrent}
-                  onClick={this.submitChoice}
-                >
-                  Submit choice
-                </button>
-              </form>
-            </div>
-          )}
-        </React.Fragment>
-      )
-    } else if (this.state.gameStatus === 'finished') {
-      return (
-        <div>
-          <h1 className="center">Results</h1>
-          <PieChart width={400} height={300}>
-            <Pie
-              data={answerData}
-              dataKey="value"
-              nameKey="name"
-              cx="50%"
-              cy="50%"
-              innerRadius={80}
-              outerRadius={100}
-              paddingAngle={0}
-              fill="#8884d8"
-            >
-              <Cell fill="#00C49F" />
-              <Cell fill="#ff3860" />
-              <Label position="center">{`${numCorrect} Correct`}</Label>
-            </Pie>
-          </PieChart>
-
-          <Navbar />
-        </div>
+        <Voting slug={this.props.slug} otherPlayers={this.state.otherPlayers} />
       )
     } else if (this.state.gameStatus === 'non-participant') {
       return <Redirect to="/" />
--- client/components/wolf/Voting.js
+++ client/components/wolf/Voting.js
@@ -1,34 +1,63 @@
 import React from 'react'
-import {getPlayersThunk, voteForPlayerThunk} from '../../store/game_helpers'
+import {voteForPlayerThunk} from '../../store/game_helpers'
 import {connect} from 'react-redux'
 
+//rewriting this to expect the players as a prop
 class Voting extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      loaded: false
+      currentChoice: false,
+      submitted: false
+    }
+    this.setChoice = this.setChoice.bind(this)
+    this.submitChoice = this.submitChoice.bind(this)
+  }
+
+  setChoice(event) {
+    this.setState({currentChoice: event.target.value})
+  }
+
+  submitChoice(event) {
+    event.preventDefault()
+    if (!this.state.submitted) {
+      this.props.voteForPlayerThunk(
+        this.props.slug,
+        this.props.user.uid,
+        this.state.currentChoice
+      )
+      this.setState({submitted: true})
     }
   }
 
-  async componentDidMount() {
-    await this.props.getPlayersThunk(this.props.slug, this.props.uid)
-    this.setState({loaded: true})
-  }
-
   render() {
+    console.log(this.props)
     return (
       <div>
-        {this.state.loaded && (
-          <div>
-            {Object.keys(this.props.game_helpers.players).map(key => {
-              return (
-                <li key={key}>
-                  {this.props.game_helpers.players[key].displayName}
-                </li>
-              )
-            })}
-          </div>
-        )}
+        <form onSubmit={this.submitChoice} onChange={this.setChoice}>
+          {Object.keys(this.props.otherPlayers).map(key => {
+            return (
+              <label key={key}>
+                <input type="radio" name="choices" value={key} />
+                <p>{this.props.otherPlayers[key].displayName}</p>
+              </label>
+            )
+          })}
+          {!this.state.submitted && (
+            <button
+              title="submit answer"
+              className="button6"
+              type="submit"
+              disabled={!this.state.currentChoice}
+              onClick={this.submitChoice}
+            >
+              Submit Choice{' '}
+            </button>
+          )}
+          {this.state.submitted && (
+            <div>You voted for {this.state.currentChoice}</div>
+          )}
+        </form>
       </div>
     )
   }
@@ -36,14 +65,12 @@ class Voting extends React.Component {
 
 const mapState = state => {
   return {
-    user: state.user,
-    game_helpers: state.game_helpers
+    user: state.user
   }
 }
 
 const mapDispatch = dispatch => {
   return {
-    getPlayersThunk: (slug, uid) => dispatch(getPlayersThunk(slug, uid)),
     voteForPlayerThunk: (slug, uid, playerId) =>
       dispatch(voteForPlayerThunk(slug, uid, playerId))
   }
