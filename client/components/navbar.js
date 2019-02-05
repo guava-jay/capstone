a44101e1bf1919271fbbeae61430b15f326118ef
Eve Mendelevich <ym9382@gmail.com>
2019-02-05 12:08:31 -0500

    style: large devices show the home buttons in two rows

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -2,24 +2,22 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Navbar = () => (
-  <div align="center">
-    <nav className="links">
-      <Link to="/join">
-        <h4>Join Game</h4>
-      </Link>
-      <Link to="/newGame">
-        <h4>Create New Game</h4>
-      </Link>
-      <Link to="/how-to-play">
-        <h4>How To Play</h4>
-      </Link>
-      <Link to="/about">
-        <h4>About</h4>
-      </Link>
-      <div className="box">
-        <img src="/img/cartoon-box.png" />
-      </div>
-    </nav>
+  <div className="links">
+    <Link to="/join">
+      <h4>Join Game</h4>
+    </Link>
+    <Link to="/newGame">
+      <h4>Create New Game</h4>
+    </Link>
+    <Link to="/how-to-play">
+      <h4>How To Play</h4>
+    </Link>
+    <Link to="/about">
+      <h4>About</h4>
+    </Link>
+    <div className="box">
+      <img src="/img/cartoon-box.png" />
+    </div>
   </div>
 )
 
